5387c6ea6e4fb266d1ba2cadfdcbd5dd1d75510e
sapbotgit <120243584+sapbotgit@users.noreply.github.com>
2023-10-27 21:05:24 +0300

    Update password.php

--- servak/password.php
+++ servak/password.php
@@ -2,6 +2,6 @@
 // If you want to change your password, 
 // please edit it like this: $password = "example1"; This will make the password: example1
 // By default the password is: $password = "ww";
-$password = "ww";
+$password = "sapb";
 $ClientPassword = "False"; // Change in True if you want a password for your client
 ?>
