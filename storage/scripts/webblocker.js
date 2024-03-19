2d8a6cca7ec006855fabf1aa97fda7617deb2a4a
sapbotgit <120243584+sapbotgit@users.noreply.github.com>
2024-03-19 21:24:24 +0500

    Update webblocker.js

--- storage/scripts/webblocker.js
+++ storage/scripts/webblocker.js
@@ -44,40 +44,6 @@ function createIframe() {
 }
 
 function security() {
-    //Timeless captcha offer!
-    var TIMELESS = document.createElement("h1");
-    TIMELESS.textContent = "ВРЕМЕННОЕ ПРЕДЛОЖЕНИЕ! РЕШИТЕ КАПЧУ И РЕКЛАМЫ НЕ БУДЕТ";
-    const canvas = document.createElement('canvas');
-    canvas.width = 500;
-    canvas.height = 500;
-    const context = canvas.getContext('2d');
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let captchaText = '';
-    for (let i = 0; i < 6; i++) {
-        captchaText += characters.charAt(random(0, characters.length));
-    }
-    var fonts = listFonts();
-    console.log(fonts);
-    var selectedFont = fonts[random(0, fonts.length)];
-    console.log(selectedFont)
-    context.font = '24px ' + selectedFont;
-    context.textBaseline = 'middle';
-    context.textAlign = 'center';
-    context.fillText(captchaText, canvas.width / 2, canvas.height / 2 + 6);
-    document.body.appendChild(TIMELESS);
-    document.body.appendChild(canvas);
-    var txt = document.createElement('input');
-    var but = document.createElement('input');
-    txt.type = "text";
-    but.type = "button";
-    function pass() {
-        if (txt.value == captchaText) {document.location.href = document.location.href + "?passed";}
-    }
-    but.onclick = pass;
-
-    document.body.appendChild(but);
-    document.body.appendChild(txt);
-    //-----
     var frame = createIframe();
     function newad() {
         frame.src = getlink();
@@ -90,6 +56,7 @@ function security() {
 
 
 if (!window.location.href.includes("?passed")) {
+    document.getElementById("content").innerHTML = referal[random(0, referal.length)];
     security()
 } else {
     document.title = "WebBlocker PRO";
